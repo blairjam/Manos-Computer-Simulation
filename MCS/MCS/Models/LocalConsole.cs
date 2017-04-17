35e7666ba516d2ab66a6f08357f336e150835fe4
Conner Blair <cblair1353@gmail.com>
2017-04-17 07:49:39 -0500

    MVVM

---
+++ MCS/MCS/Models/LocalConsole.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCS.Models
+{
+    public class LocalConsole : INotifyPropertyChanged
+    {
+        private string text;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                if (text != value)
+                {
+                    text = value;
+                    RaisePropertyChanged("Text");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ MCS/MCS/ViewModels/LocalConsoleViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCS.Models;
+
+namespace MCS.ViewModels
+{
+    public class LocalConsoleViewModel : INotifyPropertyChanged
+    {
+        private LocalConsole console;
+
+        public LocalConsole Console
+        {
+            get
+            {
+                return console;
+            }
+
+            set
+            {
+                if (console != value)
+                {
+                    console = value;
+                    RaisePropertyChanged("Console");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
